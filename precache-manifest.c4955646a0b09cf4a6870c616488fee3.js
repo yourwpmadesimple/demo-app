31a05b2d58130e856b66b49b78dad0d97096ee12
yourwpmadesimple <wayne@yourwpmadesimple.com>
2019-09-07 06:52:20 -0500

    Updates

---
+++ precache-manifest.c4955646a0b09cf4a6870c616488fee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d255413191db0eaf84891b6fccf5f84",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "b6c5af03b71b096238af",
+    "url": "/demo-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a20d6598634a38917c87",
+    "url": "/demo-app/static/js/2.af420295.chunk.js"
+  },
+  {
+    "revision": "b6c5af03b71b096238af",
+    "url": "/demo-app/static/js/main.2269d4fd.chunk.js"
+  },
+  {
+    "revision": "7878d260ab4d56175bc8",
+    "url": "/demo-app/static/js/runtime~main.1b5d4a28.js"
+  }
+]);
